5322489e9bf1c5747564828d62f59faa29c447c5
yabberyabber <andrew2nelson@gmail.com>
2015-04-15 15:05:55 -0700

    Update Parser.h

--- Parser.h
+++ Parser.h
@@ -3,4 +3,9 @@
  * a file it shouldn't matter), discard comments and whitespace, and
  * figure out whether the line contains any symbols to register or an
  * instruction to convert (or I guess a pseudoinstruction to deconstruct?)
+ * 
+ * One possibility could be to have a struct that represents a "line" of code
+ * It would have a field saying whether it was a label or an instruction
+ * and if it was an instruction the opcode and arguments fields would be 
+ * filled out.  
  */
